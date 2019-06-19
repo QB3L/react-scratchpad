14974b3de65d45cf008e0eb9ac3530c74e4fb61f
Ruben Nieves <rnieves@glgroup.com>
2019-06-19 16:06:24 -0500

    starting to add some basic routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import MyFunctionComponent from './MyFunctionComponent'
 import MyClassComponent from './MyClassComponent'
 import store from './store'
+import { Link } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
@@ -14,7 +15,11 @@ function App() {
         </p>
         <MyClassComponent></MyClassComponent>
         <MyFunctionComponent store={store}></MyFunctionComponent>
-        <button onClick={() => {store.totals = store.getTotals + 1}}>Click me</button>
+        <Link to="/flex">PRESS TO GO TO 'Flex'</Link>
+
+        <button onClick={() => {store.totals = store.getTotals + 1}}>
+          
+        </button>
       </header>
     </div>
   );
---
+++ src/FlexStuff.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class FlexStuff extends React.Component {
+  render() {
+    return <h1>Flex Stuff here</h1>
+  }
+}
+export default FlexStuff
