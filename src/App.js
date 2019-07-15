27652571d56bee08f931cc21c9d810c97a52056a
Ruben Nieves <rnieves@glgroup.com>
2019-07-15 15:22:36 -0500

    added why-did-you-render package to test and profiler from React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 import MyFunctionComponent from './components/MyFunctionComponent'
 import MyClassComponent from './components/MyClassComponent'
 import store from './store'
 import { Link } from 'react-router-dom'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido!!!
-        </p>
-        <MyClassComponent></MyClassComponent>
-        <MyFunctionComponent store={store}></MyFunctionComponent>
-        <Link to="/flex/10">PRESS TO GO TO 'Flex'</Link>
-
-        <button onClick={() => {store.totals = store.getTotals + 1}}>
-          
-        </button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Bienvenido!!!</p>
+                <MyClassComponent />
+                <MyFunctionComponent store={store} />
+                <br />
+                Links
+                <Link to="/flex/10">'Flex' Component</Link>
+                <Link to="/todo_no_hooks">TODO List (NO Hooks)</Link>
+                <Link to="/todo_with_hooks">TODO List (WITH Hooks!)</Link>
+                <Link to="/company">Company Selector Test</Link>
+                <br />
+                <br />
+                <button
+                    onClick={() => {
+                        store.totals = store.getTotals + 1
+                    }}>
+                    Press Me
+                </button>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { unstable_Profiler as Profiler } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import 'typeface-roboto'
@@ -13,13 +13,25 @@ import CompanySelector from './components/CompanySelector'
 // Routing
 import { Route, /*Link,*/ Switch, BrowserRouter as Router } from 'react-router-dom'
 
-const Notfound = () => <h1>Not found</h1>
+if (process.env.NODE_ENV !== 'production') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render')
+    whyDidYouRender(React)
+}
 
+const Notfound = () => <h1>Not found</h1>
+// This is an easy way to profile the app
+const logProfiler = (id: string, phase: string, actualTime: number, baseTime: number, startTime: number, commitTime: number) => {
+    console.log(`<${id}> render time: ${actualTime.toFixed(1)}ms`)
+}
+
+//Put Profiler in any component you wanna profile and see the time it took to render
 const routing = (
     <Router>
         <Switch>
             <Route exact path="/" component={App} />
+            {/* <Profiler id="Flex" onRender={logProfiler}> */}
             <Route path="/flex/:number" component={FlexStuff} />
+            {/* </Profiler> */}
             <Route path="/todo_no_hooks" component={TodoNOHooks} />
             <Route path="/todo_with_hooks" component={TodoWithHooks} />
             <Route path="/company" component={CompanySelector} />
